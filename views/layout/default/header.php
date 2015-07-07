58f33e36db258ca41f75ef45a37d7b3b5e0ddcfc
Ivan Cvetomirov Ivanov <slayer_word_11@outlook.com>
2015-07-07 10:42:57 +0300

    add bootstrap nav

--- views/layout/default/header.php
+++ views/layout/default/header.php
@@ -13,12 +13,30 @@
             ?>
         </title>
         
+        <link rel="stylesheet" href="/content/css/bootstrap.min.css" />
         <link rel="stylesheet" href="/content/css/style.css" />
+        
+        <script src="/content/js/jquery.min.js"></script>
+        <script src="/content/js/bootstrap.min.js"></script> 
     </head>
     <body>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/About">About</a></li>
-            </ul>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+                        <span class="sr-only">Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/">BaseFramework</a>
+                </div>
+                <div class="collapse navbar-collapse" id="navbar">
+                    <ul class="nav navbar-nav">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/About">About</a></li>
+                    </ul>
+                </div>
+            </div>
         </nav>
+        <div class="container">
